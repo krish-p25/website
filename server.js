e70d236154f49f43d9c547300f937c8a1b5bd4f7
Krish P <zcemkp0@ucl.ac.uk>
2023-10-28 01:26:06 +0100

    added basic server program

--- server.js
+++ server.js
@@ -1,6 +1,44 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const port = 5500;
 
+//Middleware
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static(path.join(__dirname)));
+app.set('etag', false)
+app.use(bodyParser.json());
+app.use(cors());
+app.disable('x-powered-by');
+
+// Middleware to set cache-control headers for all routes
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    next();
+});
+
+//HTTPS server
+https.createServer(options, app).listen(443, () => {
+    console.log(new Date().toLocaleString('en-GB', { timeZone: 'Europe/London' }), 'HTTPS Server running on port 443');
+});
+
+//HTTP server
+app.listen(80, () => {
+    console.log(new Date().toLocaleString('en-GB', { timeZone: 'Europe/London' }), 'HTTP Server running on port 80');
+});
+
+//Routes
 app.get('/', (req, res) => res.send('Hello World!'));
+
+//Error handling
+process.on('uncaughtException', (err) => {
+    console.log('Uncaught Exception')
+    console.log(new Date().toLocaleString('en-GB', { timeZone: 'Europe/London' }), err)
+})
+
+app.get('*', function (req, res) {
+    res.setHeader('x-created-by', 'kp')
+    res.send('Page 404');
+});
